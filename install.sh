cb6c56222fff6fcd0a08e8605e9fdc3a3ed29833
George Drak <sonicgd@gmail.com>
2021-02-16 16:12:53 +0500

    fix memcache installation

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pat="7.3.*"
+gdpat="7.3.*"
+memcachepat="8.*.*"
 apt update && apt install -y \
     libxml2-dev \
     zlib1g-dev \
@@ -29,7 +30,7 @@ apt update && apt install -y \
     duplicity \
     && ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/local/include/ \
     && docker-php-ext-configure gmp \
-    && if [[ $PHP_VERSION =~ $pat ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
+    && if [[ $PHP_VERSION =~ $gdpat ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
     && docker-php-ext-install pdo_pgsql pgsql soap zip xsl opcache pcntl gd bcmath pdo_mysql mysqli gmp exif intl fileinfo \
     && pecl install redis \
     && docker-php-ext-enable redis \
@@ -39,7 +40,7 @@ apt update && apt install -y \
     && docker-php-ext-enable grpc \
     && pecl install memcached-3.1.5 \
     && docker-php-ext-enable memcached \
-    && pecl install memcache-4.0.5.2 \
+    && if [[ $PHP_VERSION =~ $memcachepat ]]; then pecl install memcache-8.0; else pecl install memcache-4.0.5.2; fi \
     && docker-php-ext-enable memcache \
     && apt purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false \
     autoconf \
