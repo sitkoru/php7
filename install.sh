ea337e2f548b129c4b51262ea9dff048e9aad7fd
George Drak <sonicgd@gmail.com>
2021-03-25 13:59:35 +0500

    let's try to speedup php build and reduce final binary size

--- install.sh
+++ install.sh
@@ -13,17 +13,20 @@ apt update && apt install -y \
     libzip-dev \
     libmemcached-dev \
     libgmp-dev \
+    binutils \
     && ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/local/include/ \
     && docker-php-ext-configure gmp \
     && if [[ $PHP_VERSION =~ $gdpat ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
     && docker-php-ext-install pdo_pgsql pgsql soap zip xsl opcache pcntl gd bcmath pdo_mysql mysqli gmp exif intl fileinfo \
-    && pecl install redis \
+    && MAKEFLAGS="-j $(nproc)" pecl install redis \
     && docker-php-ext-enable redis \
-    && pecl install mongodb \
+    && MAKEFLAGS="-j $(nproc)" pecl install mongodb \
     && docker-php-ext-enable mongodb \
-    && pecl install grpc \
+    && MAKEFLAGS="-j $(nproc)" pecl install grpc \
     && docker-php-ext-enable grpc \
-    && pecl install memcached-3.1.5 \
+    && MAKEFLAGS="-j $(nproc)" pecl install memcached-3.1.5 \
     && docker-php-ext-enable memcached \
     && if [[ $PHP_VERSION =~ $memcachepat ]]; then pecl install memcache-8.0; else pecl install memcache-4.0.5.2; fi \
-    && docker-php-ext-enable memcache
+    && docker-php-ext-enable memcache \
+    # strip debug information for extensions
+    && strip --strip-debug $(php-config --extension-dir)/*.so
