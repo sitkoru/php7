b42a0bc487207a5d90cb29be4ba16b208c44e40e
George Drak <sonicgd@gmail.com>
2021-05-11 14:51:44 +0500

    use docker-php-ext-install

--- install.sh
+++ install.sh
@@ -15,17 +15,19 @@ apt update && apt install -y \
     libgmp-dev \
     binutils \
     && ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/local/include/ \
+    && docker-php-source extract \
+    && pecl bundle -d /usr/src/php/ext redis \
+    && docker-php-ext-install -j$(nproc) redis \
+    && pecl bundle -d /usr/src/php/ext mongodb \
+    && docker-php-ext-install -j$(nproc) mongodb \
+    && pecl bundle -d /usr/src/php/ext grpc \
+    && docker-php-ext-install -j$(nproc) grpc \
+    && pecl bundle -d /usr/src/php/ext memcached-3.1.5 \
+    && docker-php-ext-install -j$(nproc) memcached \
     && docker-php-ext-configure gmp \
     && if [[ $PHP_VERSION =~ $gdpat ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
     && docker-php-ext-install pdo_pgsql pgsql soap zip xsl opcache pcntl gd bcmath pdo_mysql mysqli gmp exif intl fileinfo \
-    && MAKEFLAGS="-j $(nproc)" pecl install redis \
-    && docker-php-ext-enable redis \
-    && MAKEFLAGS="-j $(nproc)" pecl install mongodb \
-    && docker-php-ext-enable mongodb \
-    && MAKEFLAGS="-j $(nproc)" pecl install grpc \
-    && docker-php-ext-enable grpc \
-    && MAKEFLAGS="-j $(nproc)" pecl install memcached-3.1.5 \
-    && docker-php-ext-enable memcached \
-    && if [[ $PHP_VERSION =~ $memcachepat ]]; then pecl install memcache-8.0; else pecl install memcache-4.0.5.2; fi \
-    && docker-php-ext-enable memcache \
+    && if [[ $PHP_VERSION =~ $memcachepat ]]; then pecl bundle -d /usr/src/php/ext memcache-8.0; else pecl bundle -d /usr/src/php/ext memcache-4.0.5.2; fi \
+    && docker-php-ext-install memcache \
+    && docker-php-source delete \
     && strip --strip-debug $(php-config --extension-dir)/*.so
